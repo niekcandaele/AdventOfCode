7ab1996dd40b3433fb175e582c71c406f67bc1c9
Niek Candaele <niekcandaele@gmail.com>
2020-12-02 16:22:32 +0100

    Add script to create day folder

---
+++ createDay.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# yes, I'm seriously too lazy to create 3 files by hand
+
+if [ $# -eq 0 ]; then
+    echo "No arguments supplied"
+    exit 1
+fi
+
+if ! [[ "$1" =~ ^[0-9]+$ ]]
+    then
+        echo "Sorry integers only"
+        exit 1
+fi
+
+
+DAY="$1"
+
+
+mkdir $DAY
+touch $DAY/challenge.md
+touch $DAY/index.js
+touch $DAY/input.js
