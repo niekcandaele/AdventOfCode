35b9e634955d599366b102148b8f2ca04d08e9f9
Niek Candaele <niekcandaele@gmail.com>
2020-12-10 18:25:55 +0100

    This is a lot cleaner :)

--- challenges/10/index.py
+++ challenges/10/index.py
@@ -3,41 +3,20 @@ from functools import lru_cache
 class Challenge():
     def __init__(self, data):
         self.input = list(map(lambda x: int(x), data.splitlines()))
-        self.chain = [0]
-        self.currentAdapter = 0
+        self.input.sort()
+        # Add our device to the input list
+        self.input.append(max(self.input) + 3)
+        # Add the outlet
+        self.input.insert(0,0)
         print(self.input)
 
     def star1(self):
-        for i in range(len(self.input)):
-            self.getAdapter()
-        print(self.chain)
-        # Add our device to the end of the chain, which is always + 3
-        self.chain.append(self.chain[len(self.chain) - 1] + 3)
-        diffs = self.calcJumps(self.chain)
-        print(f'Found {len(diffs)} diffs! With {diffs.count(1)} of 1 and {diffs.count(3)} of 3 {diffs}')
-
-        answer = diffs.count(1) * (diffs.count(3))
-        print(f'Finished! Answer is {answer}')
-        return answer 
+        diffs = self.calcJumps(self.input)
+        return diffs.count(1) * (diffs.count(3)) 
 
     def star2(self):
-        for i in range(len(self.input)):
-            self.getAdapter()
-        # Add our device to the end of the chain, which is always + 3
-        self.chain.append(self.chain[len(self.chain) - 1] + 3)
-
-        perms = self.calcPerms(max(self.chain))
+        perms = self.calcPerms(max(self.input))
         return perms
-
-    def getAdapter(self):
-        for i in range(4):
-            try:
-                self.input.remove(self.currentAdapter + i)
-                self.chain.append(self.currentAdapter + i)
-                self.currentAdapter += i
-                return self.currentAdapter + i
-            except ValueError as err:
-                pass
                 
     def calcJumps(self, list):
         return [list[n]-list[n-1] for n in range(1,len(list))]
@@ -65,6 +44,6 @@ class Challenge():
             subTotal = 0
             # Acceptable differences are 1,2 and 3
             for i in range(1, 4):
-                if x-i in self.chain: 
+                if x-i in self.input: 
                     subTotal += self.calcPerms(x-i)
             return subTotal
