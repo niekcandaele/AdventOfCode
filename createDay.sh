b63b31100f686439aa07b1d3b59806099b084817
Niek Candaele <niek@candaele.dev>
2021-12-01 17:16:37 +0100

    adjust createDay script to support multiple years

--- createDay.sh
+++ createDay.sh
@@ -2,10 +2,13 @@
 
 # yes, I'm seriously too lazy to create these files by hand
 
+DAY="$1"
+YEAR=`date +%Y`
+
 get_input() {
     # Credit: https://github.com/Bruception/advent-of-code-2020/blob/master/get_input.sh
     cookies=`cat .cookies`
-    printf '%s' "`curl -H "cookie: $cookies" https://adventofcode.com/2020/day/$DAY/input`" > challenges/$DAY/input.txt
+    printf '%s' "`curl -H "cookie: $cookies" https://adventofcode.com/$YEAR/day/$DAY/input`" > challenges/$DAY/input.txt
 }
 
 if [ $# -eq 0 ]; then
@@ -19,11 +22,9 @@ if ! [[ "$1" =~ ^[0-9]+$ ]]; then
 fi
 
 
-DAY="$1"
-
 printf "Creating files for day $DAY\n"
-mkdir challenges/$DAY
-cp -r challenges/0/* challenges/$DAY
+mkdir challenges/$YEAR/$DAY
+cp -r challenges/template/* challenges/$YEAR/$DAY
 
 printf "Getting input for day $DAY...\n"
 get_input
